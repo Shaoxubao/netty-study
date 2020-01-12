2d2f7edb389dd6e1b42d82ea7bfa65273b4bc5ea
shaoxubao <shaoxubao-sz@fangdd.com>
2020-01-12 14:58:03 +0800

    NIO demo

---
+++ src/main/java/com/baoge/netty/demo_008_nio/selector/nio/NioServer.java
@@ -0,0 +1,95 @@
+package com.baoge.netty.demo_008_nio.selector.nio;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Copyright 2018-2028 Baoge All Rights Reserved.
+ * Author: Shao Xu Bao <15818589952@163.com>
+ * Date:   2020/1/12
+ */
+public class NioServer {
+
+    private static Map<String, SocketChannel> clientMap = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(new InetSocketAddress(8899));
+
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT); // serverSocketChannel注册到selector
+
+        while (true) {
+            try {
+                int select = selector.select();// 阻塞
+
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                selectionKeys.forEach(selectionKey -> {
+                    final SocketChannel clientChannel;
+                    try {
+                        if (selectionKey.isAcceptable()) {
+                            ServerSocketChannel serverChannel = (ServerSocketChannel) selectionKey.channel();
+                            clientChannel = serverChannel.accept();
+                            clientChannel.configureBlocking(false);
+                            clientChannel.register(selector, SelectionKey.OP_READ); // 读取事件
+
+                            String key = "【" + UUID.randomUUID().toString() + "】";
+                            clientMap.put(key, clientChannel);
+                        } else if (selectionKey.isReadable()) { // 可读取事件
+                            clientChannel = (SocketChannel) selectionKey.channel();
+
+                            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                            int count = clientChannel.read(readBuffer);
+                            if (count > 0) {
+                                readBuffer.flip();
+
+                                Charset charset = Charset.forName("utf-8");
+                                String receiveMessage = String.valueOf(charset.decode(readBuffer).array());
+                                System.out.println(clientChannel + ": " + receiveMessage);
+
+                                String senderKey = null;
+                                for (Map.Entry<String, SocketChannel> entry : clientMap.entrySet()) {
+                                    if (clientChannel == entry.getValue()) {
+                                        senderKey = entry.getKey();
+                                        break;
+                                    }
+                                }
+
+                                for (Map.Entry<String, SocketChannel> entry : clientMap.entrySet()) {
+                                    SocketChannel value = entry.getValue();
+
+                                    ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                                    writeBuffer.put((senderKey + ": " + receiveMessage).getBytes());
+                                    writeBuffer.flip();
+
+                                    value.write(writeBuffer);
+                                }
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                selectionKeys.clear();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
