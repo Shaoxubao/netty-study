1dbaa40c7fd94ee42ddbbe4bf3efa21410a43776
邵绪宝 <shaoxb@yunjiglobal.com>
2020-06-16 11:40:47 +0800

    LengthFieldBasedFrameDecoder案例注释

--- src/main/java/com/baoge/netty/demo_016_encode_decode/LengthFieldBasedFrameDecoderClient.java
+++ src/main/java/com/baoge/netty/demo_016_encode_decode/LengthFieldBasedFrameDecoderClient.java
@@ -11,6 +11,24 @@ import io.netty.handler.codec.string.StringEncoder;
 /**
  * @Author shaoxubao
  * @Date 2020/6/15 16:56
+ *
+ * LengthFieldPrepender(编码)功能说明：
+ * 如果协议中的第一个字段为长度字段，netty提供了LengthFieldPrepender编码器，它可以计算当前待发送消息的二进制字节长度，将该长度添加到ByteBuf的缓冲区头中
+ * 例如：对于以下包含12个字节的报文
+ *
+ *    +----------------+
+ *    | "HELLO, WORLD" |
+ *    +----------------+
+ * 假设我们指定Length字段占用2个字节，lengthIncludesLengthFieldLength指定为false，即不包含本身占用的字节，那么Length字段的值为0x000C(即12)。
+ *
+ *   +--------+----------------+
+ *   + 0x000C | "HELLO, WORLD" |
+ *   +--------+----------------+
+ * 如果我们指定lengthIncludesLengthFieldLength指定为true，那么Length字段的值为：0x000E(即14)=Length(2)+Content字段(12)
+ *
+ *   +--------+----------------+
+ *   + 0x000E | "HELLO, WORLD" |
+ *   +--------+----------------+
  */
 public class LengthFieldBasedFrameDecoderClient {
     public static void main(String[] args) throws Exception {
@@ -29,7 +47,7 @@ public class LengthFieldBasedFrameDecoderClient {
                         // 在于server建立连接后，即发送请求报文
                         public void channelActive(ChannelHandlerContext ctx) {
                             ctx.writeAndFlush("i am request!");
-                            ctx.writeAndFlush("i am a anther request!");
+//                            ctx.writeAndFlush("i am a anther request!");
                         }
                     });
                 }
