6f2011ac59d12cce7314e4680169611a822aebcd
邵绪宝 <shaoxb@yunjiglobal.com>
2020-06-16 16:36:57 +0800

    WebSocket案例

---
+++ src/main/java/com/baoge/netty/demo_018_web_socket/WebSocketServerHandler.java
@@ -0,0 +1,133 @@
+package com.baoge.netty.demo_018_web_socket;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static io.netty.handler.codec.http.HttpUtil.isKeepAlive;
+import static io.netty.handler.codec.http.HttpUtil.setContentLength;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import static io.netty.handler.codec.rtsp.RtspResponseStatuses.BAD_REQUEST;
+
+/**
+ * @Author shaoxubao
+ * @Date 2020/6/16 16:14
+ */
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    private static final Logger logger = Logger.getLogger(WebSocketServerHandler.class.getName());
+
+    private WebSocketServerHandshaker handshaker;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 传统的HTTP接入
+        if (msg instanceof FullHttpRequest) {
+            handleHttpRequest(ctx, (FullHttpRequest) msg);
+        }
+        // WebSocket接入
+        else if (msg instanceof WebSocketFrame) {
+            handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+        }
+    }
+
+    // 连接建立时，第一次发送的肯定是一个http请求，因为将http升级websocket协议需要服务端支持，但是并不是所有的服务端都支持websocket协议，
+    // 因此服务端需要在handleHttpRequest方法中，通知客户端，服务端是否同意将HTTP协议升级成WebSocket协议。
+    // 在我们的案例中，通过WebSocketServerHandshaker的handshake方法，告诉客户端，服务端同意协议升级，此时websocket连接建立完成。
+    // 一旦websocket协议建立完成，之后的所有的请求都会走handleWebSocketFrame方法
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
+
+        // 如果HTTP解码失败，返回HHTP异常
+        if (!req.getDecoderResult().isSuccess()
+                || (!"websocket".equals(req.headers().get("Upgrade")))) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, BAD_REQUEST));
+            return;
+        }
+
+        // 构造握手响应返回，本机测试
+        WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
+                "ws://localhost:8080/websocket", null, false);
+        handshaker = wsFactory.newHandshaker(req);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory
+                    .sendUnsupportedVersionResponse(ctx.channel());
+        } else {
+            handshaker.handshake(ctx.channel(), req);
+        }
+    }
+
+    /**
+      1、判断是否是CloseWebSocketFrame 请求，如果是，关闭连接
+      2、判断是否是PingWebSocketFrame 消息，如果是维持链路的Ping消息，则构造Pong消息返回。Ping和Pong是websocket里的心跳，用来保证客户端是在线的。
+      3、判断消息是否是TextWebSocketFrame 文本消息。因为websocket消息是基于HTTP协议的，http除了支持文本消息，还是支持图片上传等复杂的二进制消息。按我们的案例中，只对文本消息进行处理。
+      4、返回应答消息
+      在handleWebSocketFrame方法中，我们使用到了webscocket中不同的帧类型，CloseWebSocketFrame、PingWebSocketFrame、TextWebSocketFrame。
+      帧类型实际上就是WebSocket协议定义的消息类型。在一个完整的WebSocket帧中，包含了一个4位的二进制Opcode字段，其用来表示消息类型(http://www.tianshouzhi.com/api/tutorials/netty/341)。
+     */
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+
+        // 判断是否是关闭链路的指令
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(),
+                    (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        // 判断是否是Ping消息
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(
+                    new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+        // 本例程仅支持文本消息，不支持二进制消息
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException(String.format(
+                    "%s frame types not supported", frame.getClass().getName()));
+        }
+
+        // 返回应答消息
+        String request = ((TextWebSocketFrame) frame).text();
+        if (logger.isLoggable(Level.FINE)) {
+            logger.fine(String.format("%s received %s", ctx.channel(), request));
+        }
+        ctx.channel().write(new TextWebSocketFrame(" 收到客户端请求：" + request));
+    }
+
+    private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse res) {
+        // 返回应答给客户端
+        if (res.getStatus().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(),
+                    CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+            setContentLength(res, res.content().readableBytes());
+        }
+
+        // 如果是非Keep-Alive，关闭连接
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (!isKeepAlive(req) || res.getStatus().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
